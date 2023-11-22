ce4fc833ad38934f802f72747a10c6543655ae52
Omars2003 <91102881+Omars2003@users.noreply.github.com>
2023-11-21 18:00:14 -0600

    Update mayus_minus_MINUS_MAYUS.asm
    salto de linea

--- mayus_minus_MINUS_MAYUS.asm
+++ mayus_minus_MINUS_MAYUS.asm
@@ -1,11 +1,12 @@
-       .model small
+     .model small
          .stack
          .data
-   NOMBRE db 'omar',10,13
-          db 'y en mayusculas  es $'
-    MAYUS db 'omar'
-    MINUS db 'OMAR',10,13 
-          db 'y en minusculas es $'
+   NOMBRE db 'Mi nombre en minusculas es omar',10,13
+          db 'y en mayusculas  es:   $'
+    MAYUS db 'omar $' 
+    
+    MINUS db 'Mi nombre en mayusculas es OMAR',10,13 
+          db 'y en minusculas es:    $'
     MIN   db  'OMAR'
     
     
@@ -20,7 +21,7 @@
            int 21h
            
            lea bx,MAYUS
-           
+                                   ;de minusculas a mayusculas       
            mov cx,4
     repite:mov ah,[bx]
            and ah,11011111b  
@@ -31,25 +32,36 @@
            int 21h
            
            inc bx
-           loop repite
+           loop repite  
+           
+           mov ah,2h
+           mov dh,2
+           mov dl,1
+           mov bh,0
+           int 10h
             
            
           
            
+                                   ;de mayusculas a minusculas         
            
+      
+           mov dx,offset MINUS  
            
+           mov ah,09  
            
-           mov dx,offset MINUS
-           mov ah,09
            int 21h
            
            lea bx,MIN 
            mov cx,4
-  repite2:
+  repite2:    
+           
   
            mov ah,[bx]
-           or ah,00100000b
-           mov dl,ah
+           or ah,00100000b 
+           
+           mov dl,ah 
+           
            mov ah,02
            int 21h  
            
